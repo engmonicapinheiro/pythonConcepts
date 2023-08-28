a67f6cf6d5a1d4297dbb77381b9c54644ea9dc56
monicapinheiro <monicafeitosa@gmail.com>
2023-08-28 10:32:30 +0100

    square.py
    Calculates the area and perimeter of a square

---
+++ square.py
@@ -0,0 +1,5 @@
+quadradoArea = input("insira o lado do quadrado: ")
+lado = float(quadradoArea)
+perimetro = int(4*lado)
+area = int(lado**2)
+print("perímetro:", perimetro, "- área:", area)  #perímetro:x - área:y
